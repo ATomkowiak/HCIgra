761cd2f910c3fbd800cdc6608e404093454a2525
ATomkowiak <50331900+ATomkowiak@users.noreply.github.com>
2020-01-16 17:29:05 +0100

    Update gra.py

--- gra.py
+++ gra.py
@@ -23,35 +23,37 @@ y = 30
 font = pygame.font.Font(None, 36)
 
 game_over = False
-def gra():
-    while not done:
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                done = True
 
-        if y > 350 or y < 0:
-            game_over = True
+while not done:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            done = True
 
-        if not game_over:
-            y += 2.5
-            pressed = pygame.key.get_pressed()
-            if pressed[pygame.K_SPACE]: y-= 12
 
-        screen.fill(BLACK)
+    if y > 350 or y < 0:
+        game_over = True
 
-        pygame.draw.rect(screen, BLUE, [x, y, 50, 50])
+    if not game_over:
+        y += 2.5
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_SPACE]: y-= 12
 
-        if game_over:
-            # If game over is true, draw game over
-            text = font.render("Game Over", True, WHITE)
-            text_rect = text.get_rect()
-            text_x = screen.get_width() / 2 - text_rect.width / 2
-            text_y = screen.get_height() / 2 - text_rect.height / 2
-            screen.blit(text, [text_x, text_y])
-            game_over=False
+    screen.fill(BLACK)
 
-        clock.tick(FPS)
+    pygame.draw.rect(screen, BLUE, [x, y, 50, 50])
 
-        pygame.display.flip()
+    if game_over:
+        # If game over is true, draw game over
+        text = font.render("Game Over", True, WHITE)
+        text_rect = text.get_rect()
+        text_x = screen.get_width() / 2 - text_rect.width / 2
+        text_y = screen.get_height() / 2 - text_rect.height / 2
+        screen.blit(text, [text_x, text_y])
+        game_over=False
 
-    pygame.quit()
+
+    clock.tick(FPS)
+
+    pygame.display.flip()
+
+pygame.quit()
